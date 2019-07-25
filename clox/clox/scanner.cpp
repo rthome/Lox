eac05e2c238f34f7af2f1897c32ecf59e92d8c1e
Raffael Thome <mail@rmt.name>
2019-07-25 17:45:17 +0200

    Implement chapter 16, "Scanning on Demand"

--- clox/clox/scanner.cpp
+++ clox/clox/scanner.cpp
@@ -4,9 +4,21 @@
 #include "common.h"
 #include "scanner.h"
 
-static bool is_at_end(ScannerState* state)
+static inline bool is_at_end(ScannerState* state)
 {
-    return state->current == '\0';
+    return *state->current == '\0';
+}
+
+static inline bool is_digit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+static inline bool is_alpha(char c)
+{
+    return (c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z')
+        || c == '_';
 }
 
 static bool match(ScannerState* state, char expected)
@@ -20,19 +32,19 @@ static bool match(ScannerState* state, char expected)
     return true;
 }
 
-static char peek(ScannerState* state)
+static inline char peek(ScannerState* state)
 {
     return *state->current;
 }
 
-static char peek_next(ScannerState* state)
+static inline char peek_next(ScannerState* state)
 {
     if (is_at_end(state))
         return '\0';
     return *(state->current + 1);
 }
 
-static char advance(ScannerState* state)
+static inline char advance(ScannerState* state)
 {
     state->current++;
     return *(state->current - 1);
@@ -85,6 +97,76 @@ static Token error_token(ScannerState* state, const char* message)
     return token;
 }
 
+static TokenType check_keyword(ScannerState* state, int start, int length, const char* rest, TokenType type)
+{
+    if (state->current - state->start == start + length &&
+        memcmp(state->start + start, rest, length) == 0)
+        return type;
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifier_type(ScannerState* state)
+{
+    switch (*state->start)
+    {
+    case 'a': return check_keyword(state, 1, 2, "nd", TOKEN_AND);
+    case 'c': return check_keyword(state, 1, 4, "lass", TOKEN_CLASS);
+    case 'e': return check_keyword(state, 1, 3, "lse", TOKEN_ELSE);
+    case 'f':
+        if (state->current - state->start > 1) {
+            switch (*(state->start + 1))
+            {
+            case 'a': return check_keyword(state, 2, 3, "lse", TOKEN_FALSE);
+            case 'o': return check_keyword(state, 2, 1, "r", TOKEN_FOR);
+            case 'u': return check_keyword(state, 2, 1, "n", TOKEN_FUN);
+            }
+        }
+        break;
+    case 'i': return check_keyword(state, 1, 1, "f", TOKEN_IF);
+    case 'n': return check_keyword(state, 1, 2, "il", TOKEN_NIL);
+    case 'o': return check_keyword(state, 1, 1, "r", TOKEN_OR);
+    case 'p': return check_keyword(state, 1, 4, "rint", TOKEN_PRINT);
+    case 'r': return check_keyword(state, 1, 5, "eturn", TOKEN_RETURN);
+    case 's': return check_keyword(state, 1, 4, "uper", TOKEN_SUPER);
+    case 't':
+        if (state->current - state->start > 1) {
+            switch (*(state->start + 1))
+            {
+            case 'h': return check_keyword(state, 2, 2, "is", TOKEN_THIS);
+            case 'r': return check_keyword(state, 2, 2, "ue", TOKEN_TRUE);
+            }
+        }
+        break;
+    case 'v': return check_keyword(state, 1, 2, "ar", TOKEN_VAR);
+    case 'w': return check_keyword(state, 1, 4, "hile", TOKEN_WHILE);
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier(ScannerState* state)
+{
+    while (is_alpha(peek(state)) || is_digit(peek(state)))
+        advance(state);
+
+    return make_token(state, identifier_type(state));
+}
+
+static Token number(ScannerState* state)
+{
+    while (is_digit(peek(state)))
+        advance(state);
+
+    if (peek(state) == '.' && is_digit(peek_next(state)))
+    {
+        advance(state);
+        while (is_digit(peek(state)))
+            advance(state);
+    }
+
+    return make_token(state, TOKEN_NUMBER);
+}
+
 static Token string(ScannerState* state)
 {
     while (peek(state) != '"' && !is_at_end(state))
@@ -118,6 +200,11 @@ Token scan_token(ScannerState* state)
         return make_token(state, TOKEN_EOF);
 
     char c = advance(state);
+
+    if (is_alpha(c))
+        return identifier(state);
+    if (is_digit(c))
+        return number(state);
 
     switch (c)
     {
